453a3b3ba07b86e0683744935debc8f4c1377b53
Aleksey Konstantinov <alex_kav@mail.ru>
2021-10-16 22:33:05 +0300

    feat: the slider change function is implemented

---
+++ js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const sliderControlPanelIndicators = document.querySelector('.slider-control__panel-indicators');
+
+// получение активного слайдера
+const activSlider = () => {
+    const sliderWrapperItem = document.querySelectorAll('.slider-wrapper__item');
+    let altText = '',
+        ind = 0;
+
+    sliderWrapperItem.forEach((sliderItem, index) => {
+        if (sliderItem.classList.contains('img-activ')) {
+            altText = sliderItem.childNodes[1].alt;
+            ind = index;
+        }
+    });
+    return [sliderWrapperItem, altText, ind];
+}
+
+//добавление подписи
+const setTextContent = () => {
+    const sliderControlPanelInfoP = document.querySelector('.slider-control__panel-info-p');
+
+    let [arr, altText, ind] = activSlider();
+
+    sliderControlPanelInfoP.style.opacity = 0;
+
+    setTimeout(() => {
+        sliderControlPanelInfoP.textContent = altText;
+        sliderControlPanelInfoP.style.opacity = 1;
+    }, 500);
+};
+
+// изменение активного слайдера
+const changingActiveSlider = (item) => {
+    let [arr, altText, ind] = activSlider();
+
+    if (ind !== item) {
+        arr[ind].classList.remove('img-activ');
+        arr[item].classList.add('img-activ');
+        setTextContent();
+    }
+
+}
+
+sliderControlPanelIndicators.addEventListener('click', (event) => {
+    let target = event.target;
+
+    console.log(target);
+
+    if (target.classList.contains('slider-control__panel-indicators-li')) {
+        if (target.dataset.slide === undefined) {
+            changingActiveSlider(target.parentNode.dataset.slide);
+        } else {
+            changingActiveSlider(target.dataset.slide);
+        }
+    }
+
+});
+
+setTextContent();
